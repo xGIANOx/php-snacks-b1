6c0b52cee63250578cfc510c699a5aae2760118b
xGIANOx <studioenricogian@gmail.com>
2023-05-04 12:11:04 +0200

    [add] matches array

---
+++ snack1/index.php
@@ -0,0 +1,36 @@
+<!-- Creiamo un array contenente le partite di basket di un’ipotetica tappa del calendario. Ogni array avrà una squadra di casa e una squadra ospite, punti fatti dalla squadra di casa e punti fatti dalla squadra ospite. Stampiamo a schermo tutte le partite con questo schema.
+
+Olimpia Milano - Cantù | 55-60 -->
+
+
+<?php
+
+    $matches = [
+        [
+            "Casa" => "Olimpia Milano",
+            "Ospite" => " Cantù",
+            "Punteggio Casa" => "55",
+            "Punteggio Ospite" => "60"
+        ],
+        [
+            "Casa" => "Virtus Padova",
+            "Ospite" => "Torino",
+            "Punteggio Casa" => "88",
+            "Punteggio Ospite" => "42"
+        ],
+    ]
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php-snack1</title>
+</head>
+<body>
+    
+</body>
+</html>
